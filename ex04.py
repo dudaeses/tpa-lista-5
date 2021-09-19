a01c27cec392db2d1452b01c28588fead4e0261e
Duda Meneses <89923722+dudaeses@users.noreply.github.com>
2021-09-19 17:19:03 -0300

    Add files via upload

---
+++ ex04.py
@@ -0,0 +1,14 @@
+# 4) Escreva um algoritmo que imprima na tela os números de 0 a 10, com exceção do 7.
+
+cont = 1
+while cont <= 6:
+    print (cont)
+    cont = cont + 1
+
+cont = 8
+while cont <= 10:
+    print (cont)
+    cont = cont + 1
+
+
+
